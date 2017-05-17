fbc7872f1e91313a69a56d157db6caacdcad0502
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-05-17 11:58:19 -0400

    Valley Fair 1.2  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Valley Fair",
                     "default_image" : "//codecloud.cdn.speedyrails.netm/sites/58d16f226e6f641c5e080000/image/jpeg/1490294450000/vfm_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "//thedistrict.mallmaverick.com/api/v2/district/social.json"
+                    "social_feed" : "//valleyfair.mallmaverick.com/api/v3/valleyfair/social.json"
                 }
