1a8f83db03835f8a50a68afa1c96477250a671ec
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-02-04 17:39:48 -0500

    Valley Fair 1.5  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Valley Fair",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/58d16f226e6f641c5e080000/image/jpeg/1490294450000/vfm_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//valleyfair.mallmaverick.com/api/v4/valleyfair/social.json"
+                }
